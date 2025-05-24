5c9e89ecab28dd63ad93d8d7a9855eefa433098e
am1rshakh <amirshayxyunusov@gmail.com>
2025-05-24 14:43:34 +0300

    lesson5_2

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def remove_every_other(arr):
-    return arr[::2]
+print('amir')
 
 
 
 
 
-
-input_array = ["Keep", "Remove", "Keep", "Remove", "Keep"]
-result = remove_every_other(input_array)
-print(result)
